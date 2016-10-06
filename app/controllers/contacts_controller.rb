50781174a614a765acca801e187c2e5ae8b1e2fd
xiongkuang <xiongkuang@gmail.com>
2016-10-06 16:28:09 -0400

    added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,12 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
 
     if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+
+      ContactMailer.contact_email(name, email, body).deliver
+
       flash[:success] = "Message sent."
       redirect_to new_contact_path
     else
@@ -17,6 +23,6 @@ class ContactsController < ApplicationController
 
   private
   def contact_params
-    params.require(:contact).permit(:name, :mail, :comments)
+    params.require(:contact).permit(:name, :email, :comments)
   end
 end
